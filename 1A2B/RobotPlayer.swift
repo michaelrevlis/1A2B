d264faf7ba468d8bb5347f2a9805760112d11d96
michaelrevlis <michaelrevlis@gmail.com>
2016-11-06 01:06:03 +0800

    create super-robot but not yet finished

---
+++ 1A2B/RobotPlayer.swift
@@ -0,0 +1,34 @@
+//
+//  RobotPlayer.swift
+//  1A2B
+//
+//  Created by MichaelRevlis on 2016/11/6.
+//  Copyright © 2016年 MichaelRevlis. All rights reserved.
+//
+
+import Foundation
+
+
+class RobotPlayer {
+    
+    let name: String
+    let ranking: Int
+    
+    init(name: String, ranking: Int) {
+        self.name = name
+        self.ranking = ranking
+    }
+    
+    // random a name
+    // random a ranking base on competitor's ranking
+    // setup a playing system at super-class, so robot needs only override paramaters from it
+    
+    func play(answer: String) {
+        
+        
+        
+        
+    }
+    
+    
+}
